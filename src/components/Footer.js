98ab0266ca23b7cde8b0622fe1ba5dfca5f7e410
Spencer Campbell <spence@twentyhertz.com>
2021-04-30 16:26:20 -0700

    Restyled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,40 @@
 import React from "react";
 import { Instagram } from "react-feather";
-import Logo from "svg/logo.svg";
+import Logo from "svg/logo-light.svg";
 import site from "site.config.json";
 import { Link } from "gatsby";
 
 export default function Footer() {
   return (
-    <footer className="container flex flex-col items-center py-8 mx-auto text-gray-600 body-font sm:flex-row">
-      <Link
-        to="/"
-        className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start"
-      >
-        <Logo className="w-40 p-2 text-white" />
-      </Link>
-      <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:pl-4 sm:py-2 sm:border-l-2 sm:border-gray-200">
-        {site.title} © 2021
-      </p>
-      <span className="inline-flex justify-center mt-4 sm:justify-start sm:ml-auto sm:mt-0">
-        <a
-          className="ml-3 text-gray-500"
-          href="https://www.instagram.com/campesinxwombcare/"
+    <footer className="bg-primary">
+      <div className="container flex flex-col items-center py-4 mx-auto text-gray-600 body-font md:flex-row">
+        <Link
+          to="/"
+          className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start"
         >
-          <span className="sr-only">Instagram</span>
-          <Instagram className="w-5 h-5" />
+          <Logo className="w-40 p-2 text-white" />
+        </Link>
+        <p className="mt-4 text-sm text-white md:ml-4 md:mt-0 md:px-4 md:py-2 md:border-l-2 md:border-r-2 md:border-gray-200">
+          {site.title} © 2021
+        </p>
+        <a
+          className="mt-4 text-sm text-white md:pl-4 md:mt-0"
+          href="https://www.spencerevison.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Site By <span className="underline">Spencer Evison</span>
         </a>
-      </span>
+        <span className="inline-flex justify-center mt-4 sm:justify-start sm:ml-auto sm:mt-0">
+          <a
+            className="ml-3 text-white"
+            href="https://www.instagram.com/campesinxwombcare/"
+          >
+            <span className="sr-only">Instagram</span>
+            <Instagram className="w-5 h-5" />
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
