2ce89bb626d9862e7791bf13dc76e1c8bbb567ee
Spencer Campbell <spence@twentyhertz.com>
2021-05-03 21:10:28 -0700

    Added insta feed via curator

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -1,13 +1,19 @@
-import React from "react";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { Component } from "react";
 
-export default function InstagramFeed() {
-  const script = document.createElement("script");
-  script.src =
-    "https://cdn.curator.io/published/3249009b-30cf-433b-8602-3a96db0b4f3e.js";
-  script.async = true;
-  document.body.appendChild(script);
-  return (
-    <div className="container" id="curator-feed-campesinx-womb-care-layout" />
-  );
+export default class InstagramFeed extends Component {
+  componentDidMount = () => {
+    if (typeof document !== "undefined") {
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.curator.io/published/3249009b-30cf-433b-8602-3a96db0b4f3e.js";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  };
+
+  render() {
+    return (
+      <div className="container" id="curator-feed-campesinx-womb-care-layout" />
+    );
+  }
 }
